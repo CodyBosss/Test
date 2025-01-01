8d1e6a44d7f66ffd4544567e57813ec9ab7f2ae1
Sudhir <sudhir.bk9@gmail.com>
2025-01-01 12:14:19 +0530

    modification done

--- file1.java
+++ file1.java
@@ -1,5 +1,10 @@
 class file1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        m1();
+    }
+
+    public static void m1() {
+        System.out.println("This is method m1");
     }
 }
