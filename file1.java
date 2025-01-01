69245507695e93f33117aae0ac3c2d6b1829b069
Sudhir <sudhir.bk9@gmail.com>
2025-01-01 18:47:25 +0530

    m2 method added in file1

--- file1.java
+++ file1.java
@@ -2,9 +2,14 @@ class file1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         m1();
+        m2();
     }
 
     public static void m1() {
         System.out.println("This is method m1");
+    }
+
+    public static void m2() {
+        System.out.println("This is m2 method from file2");
     }
 }
